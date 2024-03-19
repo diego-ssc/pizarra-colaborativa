2b990b42b1fe521f786c7fb4f1cf023c0df17fb1
Daniel Linares <daniel.linares@ciencias.unam.mx>
2024-03-19 01:43:17 -0600

    Validate auth forms

--- app/frontend/src/views/CreateAccountView.vue
+++ app/frontend/src/views/CreateAccountView.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Button } from '../components/ui/button'
 import { CardContent, CardFooter } from '../components/ui/card'
 import AuthCard from '@/components/AuthCard.vue'
@@ -10,27 +15,73 @@ const authCardProps = {
   linkTitle: 'Inicia sesión',
   linkName: 'login'
 }
+
+const formSchema = toTypedSchema(
+  z
+    .object({
+      username: z.string(),
+      email: z.string().email('El correo no es válido'),
+      password: z.string().min(8, { message: 'La contraseña debe tener 8 caracteres mínimo' }),
+      confirmPassword: z.string()
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Las contraseñas no coinciden',
+      path: ['confirmPassword'] // path of error
+    })
+)
+
+const form = useForm({
+  validationSchema: formSchema
+})
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted!', values)
+})
 </script>
 <template>
   <AuthCard v-bind="authCardProps">
-    <CardContent class="grid gap-4">
-      <div class="grid grid-cols-2 gap-6"></div>
-
-      <div class="grid gap-2">
-        <Label for="email">Nombre de usuario</Label>
-        <Input id="email" type="email" placeholder="" />
-      </div>
-      <div class="grid gap-2">
-        <Label for="email">Correo electrónico</Label>
-        <Input id="email" type="email" placeholder="m@ejemplo.com" />
-      </div>
-      <div class="grid gap-2">
-        <Label for="password">Contraseña</Label>
-        <Input id="password" type="password" />
-      </div>
-    </CardContent>
-    <CardFooter>
-      <Button class="w-full"> Regístrarse </Button>
-    </CardFooter>
+    <form @submit="onSubmit">
+      <CardContent class="grid gap-4">
+        <FormField class="grid gap-2" v-slot="{ componentField }" name="username">
+          <FormItem>
+            <FormLabel>Nombre de usuario</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField class="grid gap-2" v-slot="{ componentField }" name="email">
+          <FormItem>
+            <FormLabel>Correo electrónico</FormLabel>
+            <FormControl>
+              <Input type="email" placeholder="usuario@ejemplo.com" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField class="grid gap-2" v-slot="{ componentField }" name="password">
+          <FormItem>
+            <FormLabel>Contraseña</FormLabel>
+            <FormControl>
+              <Input type="password" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField class="grid gap-2" v-slot="{ componentField }" name="confirmPassword">
+          <FormItem>
+            <FormLabel>Confirmar contraseña</FormLabel>
+            <FormControl>
+              <Input type="password" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </CardContent>
+      <CardFooter>
+        <Button type="submit" class="w-full"> Regístrarse </Button>
+      </CardFooter>
+    </form>
   </AuthCard>
 </template>
