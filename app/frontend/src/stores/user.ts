12a3dfb6654a386e603b87460794b4e7894811ea
Daniel Linares <daniel.linares@ciencias.unam.mx>
2024-03-20 01:20:16 -0600

    Create demo home page

--- app/frontend/src/stores/user.ts
+++ app/frontend/src/stores/user.ts
@@ -1,8 +1,11 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
+import { useSessionStorage } from '@vueuse/core'
+
+const STORE_NAME = 'user'
 
 export const useUserStore = defineStore('user', () => {
-  const authToken = ref('')
+  const authToken = useSessionStorage<string>(STORE_NAME, '')
 
   const isLoggedIn = computed(() => authToken.value !== '')
 
--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -1,5 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useAPIClient, type HTTPResponse } from '@/lib/api/client'
+import { HomeEndpoint } from '@/lib/api/api'
+
+const loading = ref(false)
+const message = ref<HTTPResponse<any, any> | null>(null)
+const error = ref(null)
+
+const client = useAPIClient()
+
+async function fetchHello() {
+  error.value = message.value = null
+  loading.value = true
+
+  try {
+    const response = await client.get(HomeEndpoint)
+    message.value = response.data
+  } catch (err: any) {
+    error.value = err.toString()
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  fetchHello()
+})
+</script>
 
 <template>
-  <main>Hello world</main>
+  <div class="post">
+    <div v-if="loading" class="loading">Cargando...</div>
+
+    <div v-if="error" class="error">{{ error }}</div>
+
+    <div v-if="message" class="content">
+      <h2
+        class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+      >
+        {{ message }}
+      </h2>
+    </div>
+  </div>
 </template>
