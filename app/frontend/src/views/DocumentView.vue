d07d9bfdee625fe30e04edf5992aad692830f547
Daniel Linares <53308806+D4niel44@users.noreply.github.com>
2024-04-23 23:59:25 -0600

    Verify that the document exists before loading.

--- app/frontend/src/views/DocumentView.vue
+++ app/frontend/src/views/DocumentView.vue
@@ -3,13 +3,22 @@ import EditorProvider from '@/components/EditorProvider.vue'
 import EditorContainer from '@/components/EditorContainer.vue'
 import UndoRedoButton from '@/components/UndoRedoButton.vue';
 import { useRoute } from 'vue-router';
+import { useGet } from '@/lib/api/client';
+import { WhiteboardByIDEndpoint } from '@/lib/api/api';
+import { onMounted } from 'vue';
 
 const route = useRoute()
 const docID = route.params.id as string
+
+const { get, error, isLoading } = useGet(WhiteboardByIDEndpoint({ id: docID }))
+
+onMounted(() => get())
 </script>
 
 <template>
-  <EditorProvider :docID="docID">
+  <div v-if="isLoading">Cargando ...</div>
+  <div v-else-if="error">El documento no existe</div>
+  <EditorProvider v-else :docID="docID">
     <div class="h-full">
       <UndoRedoButton class="absolute bottom-6 right-6 z-[1001]"/>
       <EditorContainer class="h-full" />
