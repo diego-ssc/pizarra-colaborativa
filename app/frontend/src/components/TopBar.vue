a1e35fb0922692bc3fb3debe9c8941c074126534
Daniel Linares <53308806+D4niel44@users.noreply.github.com>
2024-05-06 11:01:28 -0600

    Fix alignment error in Firefox.
    The eeror is caused by unspecified length in img tag.

--- app/frontend/src/components/TopBar.vue
+++ app/frontend/src/components/TopBar.vue
@@ -3,12 +3,10 @@
 
 <template>
   <header class="p-3 flex"> 
-    <div>
-      <a class="flex h-10 ml-5 mr-5" href="/">
-        <img src="../../public/pizarra.png">
-        <img src="../../public/logo.png" alt="Logo de Pizarro">
-      </a>
-    </div>
+    <a class="flex h-10 ml-5 mr-5" href="/">
+      <img class="w-10 h-10" src="../../public/pizarra.png">
+      <img class="w-36 h-10" src="../../public/logo.png" alt="Logo de Pizarro">
+    </a>
     <slot></slot>
   </header>
 </template>
