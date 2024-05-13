cb57b498ce72a37e5b5efed83e002d5c06d26b7d
Daniel Linares <53308806+D4niel44@users.noreply.github.com>
2024-05-12 20:42:00 -0600

    Implement awareness in editor

--- app/frontend/src/components/EditorProvider.vue
+++ app/frontend/src/components/EditorProvider.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useEditor, } from '@/lib/editor/editor'
 import { sync } from '@/lib/editor/sync'
+import { useUserStore } from '@/stores/user';
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue'
 
 const props = defineProps<{
@@ -10,15 +12,20 @@ const props = defineProps<{
 const editor = useEditor()
 const synced = ref(false)
 
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
+
 function switchDoc (id: any) {
   synced.value = false
   let doc = editor.collection.getDoc(id)
   if (!doc) {
     doc = editor.loadDoc(id)
   }
+  console.log(editor.collection.awarenessStore.awareness)
+  editor.collection.awarenessStore.awareness.setLocalStateField('user', { name:  user.value.username })
   sync(editor.editor, doc, id, () => {
     synced.value = true
-  })
+  }, editor.collection.awarenessStore.awareness)
 }
 
 switchDoc(props.docID)
