09dd01d736fba3be698b2dfd3ed9d1f69b0a13bb
ulisessap <ulisessap12@ciencias.unam.mx>
2024-05-20 11:00:34 -0600

    change name in editor view

--- app/frontend/src/components/EditorTopBar.vue
+++ app/frontend/src/components/EditorTopBar.vue
@@ -1,57 +1,56 @@
 <script setup lang="ts">
-import { type GetWhiteboardsResponse, WhiteboardEndpoint, WhiteboardByIDEndpoint, } from '@/lib/api/api';
-import { onMounted, ref } from 'vue';
+import { type GetWhiteboardsResponse, WhiteboardEndpoint, WhiteboardByIDEndpoint } from '@/lib/api/api';
+import { onMounted, ref, watch } from 'vue';
 import { useAPIClient, useGet, usePatch } from '@/lib/api/client';
+import { useRoute } from 'vue-router'; 
 
-const { get, data } = useGet<GetWhiteboardsResponse>(WhiteboardEndpoint)
-const client = useAPIClient()
-const newName = ref('')
-const editing = ref(false)
+const { get, data } = useGet<GetWhiteboardsResponse>(WhiteboardEndpoint);
+const client = useAPIClient();
+const route = useRoute(); 
+const boardId = route.params.id as string; 
+const name = ref('');
+const showName = ref(false);
 
+function currentWhiteboard(boardId: string): void {
+    showName.value = true;
+    name.value = data.value!.find(board => board.whiteBoardId === boardId)?.title || ''
+}
 
-async function editTitle(boardId: string) {
+async function saveNewName(boardId: string) {
     try {
-        await client.patch(WhiteboardByIDEndpoint({ id: boardId }), { title: newName.value })
+        await client.patch(WhiteboardByIDEndpoint({ id: boardId }), { title: name.value })
         get()
+        showName.value = false;
     } catch (error) {
         console.error('Error al guardar el nuevo nombre:', error)
     }
 }
 
-function start_editing() {
-    editing.value = true
-}
 
-function saveTitle(){
 
-}
+onMounted(() => {
+    get();    
+});
 
-function toggleEditing() {
-    editing.value = !editing.value;
-}
+
 </script>
 
 <template>
-
     <div class="flex w-full w-fit rounded-xl border bg-card shadow">
         <div>
             <img class="ml-8 mt-3 w-36 h-10" src="../../public/logo.png" alt="Logo de Pizarro">
-
         </div>
         <router-link to="/home">
-            <div class="left-content"> 
+            <div class="left-content">
                 <button class="px-3 py-2 bg-cyan-500 m-4 text-black font-bold rounded-xl shadow hover:bg-cyan-600">Mis pizarras</button>
             </div>
         </router-link>
+        <button class="px-3 py-2 bg-green-400 m-4 text-black font-bold rounded-xl shadow hover:bg-green-600" @click="currentWhiteboard(boardId)">Cambiar nombre</button>
+
+        <div v-if="showName">
+        <input class="border border-black rounded-xl m-4 text-center mt-6" type="text" id="title-input" v-model="name" @keydown.enter="saveNewName(boardId)"/>
+        </div>
         
-        <div v-if="!editing" @click="startEditing">{{ newName }}</div>
-        <input class="border border-black rounded-xl m-2 mt-6 ml-10" type="text" v-model="newName">
-            
-
         <button class="px-3 py-2 bg-amber-400 m-4 text-black font-bold rounded-xl shadow hover:bg-amber-600 ml-auto">Compartir</button>
-
     </div>
 </template>
-
-
-
