e0258ec81c65c07b4d6e79ac3d90e2f5ed9dc05a
diego-ssc <diego_ssc@ciencias.unam.mx>
2024-05-25 19:54:23 -0600

    Permissions applied to whiteboard interactions

--- app/backend/src/white-board/white-board.module.ts
+++ app/backend/src/white-board/white-board.module.ts
@@ -3,9 +3,10 @@ import { WhiteBoardController } from './white-board.controller';
 import { WhiteBoardService } from './white-board.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WhiteBoard } from './white-board.entity';
+import { HasPermissionModule } from 'src/has-permission/has-permission.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WhiteBoard])],
+  imports: [TypeOrmModule.forFeature([WhiteBoard]), HasPermissionModule],
   controllers: [WhiteBoardController],
   providers: [WhiteBoardService],
   exports: [WhiteBoardService],
