b462cf33526b11d774aa68a04b4b8b77808fe829
Daniel Linares <53308806+D4niel44@users.noreply.github.com>
2024-05-30 22:06:00 -0600

    Add e2e test for auth

---
+++ e2e/tests/auth.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('register and then login', async ({ page }) => {
+  const userSuffix = new Date().getTime().toString()
+
+  const username = `e2e-${userSuffix}`
+  const email = `e2e-${userSuffix}@mail.com`
+
+  await page.goto('http://localhost:8080/');
+  await page.getByRole('button', { name: 'Regístrate' }).click();
+  await page.getByLabel('Nombre de usuario').click();
+  await page.getByLabel('Nombre de usuario').fill(username);
+  await page.getByLabel('Nombre de usuario').press('Tab');
+  await page.getByLabel('Correo electrónico').fill(email);
+  await page.getByLabel('Correo electrónico').press('Tab');
+  await page.getByLabel('Contraseña', { exact: true }).fill('12345678');
+  await page.getByLabel('Confirmar contraseña').click();
+  await page.getByLabel('Confirmar contraseña').fill('12345678');
+  await page.getByRole('button', { name: 'Regístrarse' }).click();
+  await expect(page.getByRole('link', { name: 'Nueva pizarra' })).toBeVisible();
+  await page.getByRole('button', { name: 'Cerrar sesión' }).click();
+  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+  await page.getByLabel('Correo electrónico').click();
+  await page.getByLabel('Correo electrónico').fill(email);
+  await page.getByLabel('Correo electrónico').press('Tab');
+  await page.getByLabel('Contraseña').fill('12345678');
+  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+  await expect(page.getByRole('link', { name: 'Nueva pizarra' })).toBeVisible();
+  await expect(page.getByPlaceholder('Buscar...')).toBeVisible();
+  await expect(page.getByPlaceholder('Buscar...')).toBeEmpty();
+});
