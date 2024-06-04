2db3f881708854d6fb2753aa779a06d406511696
Daniel Linares <53308806+D4niel44@users.noreply.github.com>
2024-06-04 15:35:13 -0600

    Refactor: Move reusable logic from ShareByEmail to parent.

--- app/frontend/src/components/ShareByEmailForm.vue
+++ app/frontend/src/components/ShareByEmailForm.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { type HasAccessResponse, PermissionByIDEndpoint, type Action, type AddPermissionRequest } from '@/lib/api/api';
-import { useGet, usePost } from '@/lib/api/client';
-import { ref, onMounted, watch } from 'vue';
+import { PermissionByIDEndpoint, type Action, type AddPermissionRequest } from '@/lib/api/api';
+import { usePost } from '@/lib/api/client';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { Loader2 } from 'lucide-vue-next'
 import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/components/ui/tags-input'
@@ -19,10 +19,6 @@ const emit = defineEmits(['shared'])
 
 const route = useRoute(); 
 const docID = route.params.id as string;
-const { get, data, error, isLoading } = useGet<HasAccessResponse>(PermissionByIDEndpoint({ id: docID }))
-onMounted(() => {
-  get()
-})
 
 const emails = ref<string[]>([])
 const selectedAction = ref<Action>('Read')
@@ -44,57 +40,44 @@ watch(postClient.data, (value) => {
 </script>
 
 <template>
-  <div v-if="error">
-    Ha ocurrido un error
+  <div v-if="postClient.error.value" class="text-red-500">
+    Los correos introducidos no corresponden a usuarios registrados
   </div>
-  <div v-else-if="isLoading">
-    Cargando...
+  <div v-if="emails.length === 0">
+    Introduzca un correo y presione enter
   </div>
-  <div v-else-if="data === 'Admin'">
-    <div v-if="postClient.error.value" class="text-red-500">
-      Los correos introducidos no corresponden a usuarios registrados
-    </div>
+  <TagsInput v-model="emails" class="my-4">
+    <TagsInputItem v-for="item in emails" :key="item" :value="item">
+      <TagsInputItemText />
+      <TagsInputItemDelete />
+    </TagsInputItem>
+    <TagsInputInput placeholder="ejemplo@correo.com" />
+  </TagsInput>
+  <div class="flex justify-between my-4">
+    <Select v-model="selectedAction">
+      <SelectTrigger class="w-[180px]">
+        <SelectValue placeholder="Lector" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup class="z-[1001]">
+          <SelectItem value="Read">Lector</SelectItem>
+          <SelectItem value="Write">Editor</SelectItem>
+          <SelectItem value="Admin">Admin</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+
     <div v-if="emails.length === 0">
-      Introduzca un correo y presione enter
+      <Button disabled @click="shareDoc">Compartir</Button>
     </div>
-     <TagsInput v-model="emails" class="my-4">
-        <TagsInputItem v-for="item in emails" :key="item" :value="item">
-          <TagsInputItemText />
-          <TagsInputItemDelete />
-        </TagsInputItem>
-
-        <TagsInputInput placeholder="ejemplo@correo.com" />
-      </TagsInput>
-    <div class="flex justify-between my-4">
-      <Select v-model="selectedAction">
-        <SelectTrigger class="w-[180px]">
-          <SelectValue placeholder="Lector" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup class="z-[1001]">
-            <SelectItem value="Read">Lector</SelectItem>
-            <SelectItem value="Write">Editor</SelectItem>
-            <SelectItem value="Admin">Admin</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-
-      <div v-if="emails.length === 0">
-        <Button disabled @click="shareDoc">Compartir</Button>
-      </div>
-      <div v-else-if="postClient.isLoading.value">
-          <Button disabled>
-            <Loader2 class="w-4 h-4 mr-2 animate-spin" />
-            Compartiendo
-          </Button>
-      </div>
-      <div v-else>
-        <Button @click="shareDoc">Compartir</Button>
-      </div>
+    <div v-else-if="postClient.isLoading.value">
+      <Button disabled>
+        <Loader2 class="w-4 h-4 mr-2 animate-spin" />
+        Compartiendo
+      </Button>
     </div>
-
-  </div>
-  <div v-else>
-    Solo los administradores pueden cambiar el acceso
+    <div v-else>
+      <Button @click="shareDoc">Compartir</Button>
+    </div>
   </div>
 </template>
--- app/frontend/src/components/ShareDocumentDialog.vue
+++ app/frontend/src/components/ShareDocumentDialog.vue
@@ -8,8 +8,11 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import ShareByEmailForm from './ShareByEmailForm.vue'
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useToast } from './ui/toast';
+import { useRoute } from 'vue-router';
+import { useGet } from '@/lib/api/client';
+import { PermissionByIDEndpoint, type HasAccessResponse } from '@/lib/api/api';
 
 const open = ref(false)
 
@@ -21,12 +24,19 @@ function onShared() {
     description: 'Pizarra Compartida',
   })
 }
+
+const route = useRoute(); 
+const docID = route.params.id as string;
+const { get, data, error, isLoading } = useGet<HasAccessResponse>(PermissionByIDEndpoint({ id: docID }))
+onMounted(() => {
+  get()
+})
 </script>
 
 <template>
   <Dialog v-model:open="open">
     <DialogTrigger as-child>
-      <Button variant="outline">
+      <Button variant="outline" @click="get">
         Compartir
       </Button>
     </DialogTrigger>
@@ -34,7 +44,17 @@ function onShared() {
       <DialogHeader>
         <DialogTitle>Compartir pizarra</DialogTitle>
       </DialogHeader>
-      <ShareByEmailForm @shared="onShared"/>
+      <div v-if="error" class="text-red-500">
+      </div>
+      <div v-else-if="isLoading">
+        Cargando...
+      </div>
+      <div v-else-if="data === 'Admin'">
+        <ShareByEmailForm @shared="onShared"/>
+      </div>
+      <div v-else>
+        Solo los administradores pueden cambiar el acceso
+      </div>
     </DialogContent>
   </Dialog>
 </template>
